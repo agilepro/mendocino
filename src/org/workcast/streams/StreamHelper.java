55fcb156b4694225e2c84c95259471137b4ef8bd
Keith Swenson <github@kswenson.oib.com>
2015-11-25 07:24:39 +0900

    added javadoc to StreamHelper, and added file to file copy

--- src/org/workcast/streams/StreamHelper.java
+++ src/org/workcast/streams/StreamHelper.java
@@ -19,6 +19,10 @@ import java.io.Writer;
  */
 public class StreamHelper {
 
+    /**
+    * copyReaderToWriter will read character from the Reader, and send them to the
+    * Writer until the Reader is exhausted.
+    */
     public static void copyReaderToWriter(Reader r, Writer w) throws Exception {
         char[] buf = new char[1000];
         int amt = r.read(buf, 0, 1000);
@@ -29,6 +33,10 @@ public class StreamHelper {
         w.flush();
     }
 
+    /**
+    * copyInputToOutput will read bytes from the input stream, and send them to the
+    * output stream until the input stream is exhausted.
+    */
     public static void copyInputToOutput(InputStream is, OutputStream os) throws Exception {
         byte[] buf = new byte[1000];
         int amt = is.read(buf, 0, 1000);
@@ -39,6 +47,21 @@ public class StreamHelper {
         os.flush();
     }
 
+    /**
+    * Given an output stream, copyFileToOutput will read the contents of a file
+    * and stream those contents to the output stream until the entire file is sent.
+    */
+    public static void copyFileToOutput(File file, OutputStream os) throws Exception {
+        FileInputStream fis = new FileInputStream(file);
+        copyInputToOutput(fis, os);
+        fis.close();
+    }
+
+    /**
+    * Given an output Writer object, copyFileToWriter will read the contents of a file
+    * converting bytes to characters according to specified encoding, and stream those
+    * characters to the Reader until the entire file is sent.
+    */
     public static void copyFileToWriter(File file, Writer w,
             String fileEncoding) throws Exception {
         FileInputStream fis = new FileInputStream(file);
@@ -49,16 +72,24 @@ public class StreamHelper {
         fis.close();
     }
 
+    /**
+    * Given an output Writer object, copyUTF8FileToWriter will read the contents of a file
+    * converting bytes to characters according to UTF-8 encoding, and stream those
+    * characters to the Reader until the entire file is sent.
+    */
     public static void copyUTF8FileToWriter(File file, Writer w) throws Exception {
         copyFileToWriter(file, w, "UTF-8");
     }
 
-    public static void streamFileToOutput(File file, OutputStream os) throws Exception {
-        FileInputStream fis = new FileInputStream(file);
-        copyInputToOutput(fis, os);
-        fis.close();
-    }
-
+    /**
+    * copyStreamToFile will read the input stream to the end, and write the
+    * contents to the designated file.  Bytes are read and bytes are written
+    * the resulting file is in whatever encoding that the source stream is.
+    *
+    * The contents are written first to a temporary file, and then that file is
+    * renamed to destination file.  If the process is interrupted in the middle
+    * of the copy, the temporary file might remain.
+    */
     public static void copyStreamToFile(InputStream is, File file) throws Exception {
 
         File folder = file.getParentFile();
@@ -79,10 +110,28 @@ public class StreamHelper {
         tempFile.renameTo(file);
     }
 
+    /**
+    * copyReaderToUTF8File will read the input Reader to the end, and write the
+    * contents to the designated file using UTF-8 encoding, overwriting any
+    * existing file that might be there.
+    *
+    * The contents are written first to a temporary file, and then that file is
+    * renamed to destination file.  If the process is interrupted in the middle
+    * of the copy, the temporary file might remain.
+    */
     public static void copyReaderToUTF8File(Reader r, File file) throws Exception {
         copyReaderToFile(r,file,"UTF-8");
     }
 
+    /**
+    * copyReaderToUTF8File will read the input Reader to the end, and write the
+    * contents to the designated file using specified encoding, overwriting any
+    * existing file that might be there.
+    *
+    * The contents are written first to a temporary file, and then that file is
+    * renamed to destination file.  If the process is interrupted in the middle
+    * of the copy, the temporary file might remain.
+    */
     public static void copyReaderToFile(Reader r, File file, String encoding) throws Exception {
 
         File folder = file.getParentFile();
@@ -104,6 +153,24 @@ public class StreamHelper {
         tempFile.renameTo(file);
     }
 
+    /**
+    * This is a convenience routine to copy the contents of one file to another file.
+    * It does do by reading all the bytes from one, and writing the butes to another.
+    * There is no restriction on where those files might exist, just as long as they
+    * can be opened and read/writen to.
+    *
+    * The contents are written first to a temporary file, and then that file is
+    * renamed to destination file.  If the process is interrupted in the middle
+    * of the copy, the temporary file might remain.
+    */
+    public static void copyFileToFile(File inFile, File outFile) throws Exception {
+        if (!inFile.exists()) {
+            throw new Exception("Can not copy a file that does not exist: "+inFile);
+        }
+        FileInputStream fis = new FileInputStream(inFile);
+        copyStreamToFile(fis, outFile);
+        fis.close();
+    }
 
 
 }
